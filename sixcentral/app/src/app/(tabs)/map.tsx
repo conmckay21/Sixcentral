280ec399a6cadecaffd18c93780464e988954b38
Connor McKay <connor@tradespa.co.uk>
2026-07-03 12:03:47 +0100

    Map: remove detectRetina tile trap, cache-bust embed

--- sixcentral/app/src/app/(tabs)/map.tsx
+++ sixcentral/app/src/app/(tabs)/map.tsx
@@ -61,7 +61,7 @@ export default function MapTab() {
       ) : (
         <WebView
           ref={web}
-          source={{ uri: `${SITE}/map-embed.html` }}
+          source={{ uri: `${SITE}/map-embed.html?v=3` }}
           style={st.web}
           onMessage={(e) => {
             try {
